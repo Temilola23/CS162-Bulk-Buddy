676fb052fb4e1518ec0c6dc6e39b7c4ac3376536
Ekene Azubuko <ekeneazubuko2935@gmail.com>
2026-03-11 02:08:11 -0700

    Fixed black formatting

--- backend/app/routes/driver.py
+++ backend/app/routes/driver.py
@@ -2,7 +2,6 @@ from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 from app.services import create_driver_application
 
-
 driver = Blueprint("driver", __name__, url_prefix="/api/driver")
 
 
